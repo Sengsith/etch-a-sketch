353e857f6d5e694d9d03102a2c04f2640fed9fb7
Angelo Rosario <angelorosario10@gmail.com>
2024-03-25 14:48:52 -0700

    update script.js
    adjusted user input protection, no need for loop

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const createGrid = () => {
-  const CONTAINER = document.querySelector(".container");
-  const ROWS = 16;
-  const COLS = 16;
-
-  for (let i = 0; i < ROWS; i++) {
-    const NEW_ROW = document.createElement("div");
-    NEW_ROW.classList.add("row");
-    for (let j = 0; j < COLS; j++) {
-      const NEW_SQUARE = document.createElement("div");
-      NEW_SQUARE.classList.add("square");
-      NEW_ROW.appendChild(NEW_SQUARE);
+const createGrid = (container, gridSize = 16) => {
+  for (let i = 0; i < gridSize; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("row");
+    for (let j = 0; j < gridSize; j++) {
+      const newSquare = document.createElement("div");
+      newSquare.classList.add("square");
+      newRow.appendChild(newSquare);
     }
-    CONTAINER.appendChild(NEW_ROW);
+    container.appendChild(newRow);
   }
 
   const CLASS_HOVERED = "hovered";
-  CONTAINER.addEventListener("mouseover", (e) => {
+  container.addEventListener("mouseover", (e) => {
     // Only run if we hovered on a square
     if (e.target.className === "square") {
       e.target.classList.add(CLASS_HOVERED);
@@ -23,6 +19,25 @@ const createGrid = () => {
   });
 };
 
-const changeGrid = () => {};
+const removeGrid = () => {
+  container.textContent = "";
+};
 
-createGrid();
+// Create our initial grid of 16x16
+const container = document.querySelector(".container");
+createGrid(container);
+
+const button = document.querySelector(".create-grid");
+button.addEventListener("click", () => {
+  let enteredGridSize = 0;
+  enteredGridSize = parseInt(
+    prompt("Please enter a number between 1 and 100: ")
+  );
+  if (isNaN(enteredGridSize) || enteredGridSize < 1 || enteredGridSize > 100) {
+    alert("Invalid input, please enter a number between 1 and 100!");
+    return;
+  }
+
+  removeGrid();
+  createGrid(container, enteredGridSize);
+});
