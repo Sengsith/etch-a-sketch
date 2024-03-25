7390f478a8367c882f3d5228e4235896a124276c
Angelo Rosario <angelorosario10@gmail.com>
2024-03-25 13:37:22 -0700

    update README.md script.js style.css
    added hover effect

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # etch-a-sketch
+
+- [Thoughts During Attempt](#thoughts-during-attempt)
+
+## Reflections during project
+
+Turns out that after creating my squares there was empty margin under each row, even though there I set `box-sizing: border-box`, `margin: 0`, and `padding: 0`. The fix for this is simply:
+
+```css
+.row {
+  font-size: 0;
+}
+```
+
+[BACK TO TOP](#etch-a-sketch)
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const CONTAINER = document.querySelector(".container");
-const ROWS = 16;
-const COLS = 16;
-
 const createGrid = () => {
+  const CONTAINER = document.querySelector(".container");
+  const ROWS = 16;
+  const COLS = 16;
+
   for (let i = 0; i < ROWS; i++) {
     const NEW_ROW = document.createElement("div");
     NEW_ROW.classList.add("row");
@@ -13,6 +13,14 @@ const createGrid = () => {
     }
     CONTAINER.appendChild(NEW_ROW);
   }
+
+  const CLASS_HOVERED = "hovered";
+  CONTAINER.addEventListener("mouseover", (e) => {
+    // Only run if we hovered on a square
+    if (e.target.className === "square") {
+      e.target.classList.add(CLASS_HOVERED);
+    }
+  });
 };
 
 createGrid();
